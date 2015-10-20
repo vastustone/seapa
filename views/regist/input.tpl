0a9df68b6a2788e701c99e4c86deb749e480a4e7
saramune <si17erad0@gmail.com>
2015-10-20 22:55:00 +0900

    ユーザ情報登録フォームのJavascript追加

--- views/regist/input.tpl
+++ views/regist/input.tpl
@@ -2,6 +2,43 @@
 <html lang="ja">
 <head>
 <meta charset="UTF-8">
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script type="text/javascript">
+$(function(){
+  $("input[type='submit']").click(function(){
+    // エラーメッセージ初期化
+    $("#error_user").text('');
+    $("#error_pass").text('');
+    $("#error_pass_conf").text('');
+    var errorFlg = false;
+
+    // 入力チェック
+    if($("input[name='username']").val() == "") {
+      $("#error_user").text('ユーザ名が入力されていません。');
+      errorFlg = true;
+    }
+
+    if($("input[name='password']").val() == "") {
+      $("#error_pass").text('パスワードが入力されていません。');
+      errorFlg = true;
+    } else if($("input[name='password']").val().length < 6 || $("input[name='password']").val().length > 16){
+      $("#error_pass").text('パスワードは6文字以上16文字以下にしてください。');
+      errorFlg = true;
+    }
+    if($("input[name='password_conf']").val() == "") {
+      $("#error_pass_conf").text('パスワードが入力されていません。');
+      errorFlg = true;
+    } else if($("input[name='password']").val() != $("input[name='password_conf']").val()){
+      $("#error_pass_conf").text('パスワードが誤っています。');
+      errorFlg = true;
+    }
+
+    if (errorFlg) {
+      return false;
+    }
+  });
+});
+</script>
 </head>
 <body>
 {if isset($errorFlg)}
@@ -19,15 +56,15 @@
     <caption>ユーザ情報登録フォーム</caption>
     <tr>
       <td class="item">ユーザ名:</td>
-      <td><input type="text" size="30" name="username" value=""></td>
+      <td><input type="text" size="30" name="username" value=""><div id="error_user"></div></td>
     </tr>
     <tr>
       <td class="item">パスワード:</td>
-      <td><input type="password" size="30" name="password" value="">&nbsp;&nbsp;* 6文字以上16文字以下</td>
+      <td><input type="password" size="30" name="password" value="">&nbsp;&nbsp;* 6文字以上16文字以下<div id="error_pass"></div></td>
     </tr>
     <tr>
       <td class="item">パスワード(確認用):</td>
-      <td><input type="password" size="30" name="password_conf" value=""></td>
+      <td><input type="password" size="30" name="password_conf" value=""><div id="error_pass_conf"></div></td>
     </tr>
     <tr>
       <td class="item">E-mail:</td>
--- views/templates_c/1ae5fad6d29e601e9ed0fb22871f3d1098b098b0_0.file.input.tpl.php
+++ views/templates_c/1ae5fad6d29e601e9ed0fb22871f3d1098b098b0_0.file.input.tpl.php
@@ -1,7 +1,7 @@
-<?php /* Smarty version 3.1.27, created on 2015-10-20 21:45:13
+<?php /* Smarty version 3.1.27, created on 2015-10-20 22:52:13
          compiled from "/Applications/MAMP/root/public_html/seapa/views/regist/input.tpl" */ ?>
 <?php
-/*%%SmartyHeaderCode:105903971156263759229e38_77957813%%*/
+/*%%SmartyHeaderCode:11253286195626470de8f7e3_90335440%%*/
 if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
   'file_dependency' => 
@@ -9,11 +9,11 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     '1ae5fad6d29e601e9ed0fb22871f3d1098b098b0' => 
     array (
       0 => '/Applications/MAMP/root/public_html/seapa/views/regist/input.tpl',
-      1 => 1445345106,
+      1 => 1445349131,
       2 => 'file',
     ),
   ),
-  'nocache_hash' => '105903971156263759229e38_77957813',
+  'nocache_hash' => '11253286195626470de8f7e3_90335440',
   'variables' => 
   array (
     'errorFlg' => 0,
@@ -22,18 +22,59 @@ $_valid = $_smarty_tpl->decodeProperties(array (
   ),
   'has_nocache_code' => false,
   'version' => '3.1.27',
-  'unifunc' => 'content_562637593653b0_84451810',
+  'unifunc' => 'content_5626470e059371_69279333',
 ),false);
 /*/%%SmartyHeaderCode%%*/
-if ($_valid && !is_callable('content_562637593653b0_84451810')) {
-function content_562637593653b0_84451810 ($_smarty_tpl) {
+if ($_valid && !is_callable('content_5626470e059371_69279333')) {
+function content_5626470e059371_69279333 ($_smarty_tpl) {
 
-$_smarty_tpl->properties['nocache_hash'] = '105903971156263759229e38_77957813';
+$_smarty_tpl->properties['nocache_hash'] = '11253286195626470de8f7e3_90335440';
 ?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
 <meta charset="UTF-8">
+<?php echo '<script'; ?>
+ type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"><?php echo '</script'; ?>
+>
+<?php echo '<script'; ?>
+ type="text/javascript">
+$(function(){
+  $("input[type='submit']").click(function(){
+    // エラーメッセージ初期化
+    $("#error_user").text('');
+    $("#error_pass").text('');
+    $("#error_pass_conf").text('');
+    var errorFlg = false;
+
+    // 入力チェック
+    if($("input[name='username']").val() == "") {
+      $("#error_user").text('ユーザ名が入力されていません。');
+      errorFlg = true;
+    }
+
+    if($("input[name='password']").val() == "") {
+      $("#error_pass").text('パスワードが入力されていません。');
+      errorFlg = true;
+    } else if($("input[name='password']").val().length < 6 || $("input[name='password']").val().length > 16){
+      $("#error_pass").text('パスワードは6文字以上16文字以下にしてください。');
+      errorFlg = true;
+    }
+    if($("input[name='password_conf']").val() == "") {
+      $("#error_pass_conf").text('パスワードが入力されていません。');
+      errorFlg = true;
+    } else if($("input[name='password']").val() != $("input[name='password_conf']").val()){
+      $("#error_pass_conf").text('パスワードが誤っています。');
+      errorFlg = true;
+    }
+
+    if (errorFlg) {
+      return false;
+    }
+  });
+});
+<?php echo '</script'; ?>
+>
 </head>
 <body>
 <?php if (isset($_smarty_tpl->tpl_vars['errorFlg']->value)) {?>
@@ -52,15 +93,15 @@ $_smarty_tpl->properties['nocache_hash'] = '105903971156263759229e38_77957813';
     <caption>ユーザ情報登録フォーム</caption>
     <tr>
       <td class="item">ユーザ名:</td>
-      <td><input type="text" size="30" name="username" value=""></td>
+      <td><input type="text" size="30" name="username" value=""><div id="error_user"></div></td>
     </tr>
     <tr>
       <td class="item">パスワード:</td>
-      <td><input type="password" size="30" name="password" value="">&nbsp;&nbsp;* 6文字以上16文字以下</td>
+      <td><input type="password" size="30" name="password" value="">&nbsp;&nbsp;* 6文字以上16文字以下<div id="error_pass"></div></td>
     </tr>
     <tr>
       <td class="item">パスワード(確認用):</td>
-      <td><input type="password" size="30" name="password_conf" value=""></td>
+      <td><input type="password" size="30" name="password_conf" value=""><div id="error_pass_conf"></div></td>
     </tr>
     <tr>
       <td class="item">E-mail:</td>
